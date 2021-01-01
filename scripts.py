128272dc165645b8b4105691aa8dfc1377c72329
Andrew Muller <andrew.muller@utexas.edu>
2020-12-31 19:03:09 -0600

    baseline models

--- scripts.py
+++ scripts.py
@@ -1,10 +1,13 @@
 from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
 
-def print_metrics(y_test, X_test, model):
+def get_metrics(y_test, X_test, model):
     labels = y_test.to_numpy()
     preds = model.predict(X_test)
     
-    print("Accuracy Score: {}".format(accuracy_score(labels, preds)))
-    print("F1 Score: {}".format(f1_score(labels, preds, average='weighted')))
-    print("Precision Score: {}".format(precision_score(labels, preds, average='weighted')))
-    print("Recall Score: {}".format(recall_score(labels, preds, average='weighted')))
+    metrics = {}
+    metrics['accuracy'] = accuracy_score(labels, preds)
+    metrics['f1'] = f1_score(labels, preds, average='weighted')
+    metrics['precision'] = precision_score(labels, preds, average='weighted')
+    metrics['recall'] = recall_score(labels, preds, average='weighted')
+    
+    return metrics
